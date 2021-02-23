a0b6cf648d8021d4819e3889d666465091c92509
jk-collab <79513011+jk-collab@users.noreply.github.com>
2021-02-23 12:21:55 +0100

    Test

--- Test/src/Test.cpp
+++ Test/src/Test.cpp
@@ -10,6 +10,6 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello from JK-111 World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
